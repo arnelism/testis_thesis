9af56c70676abf5acee369aeae4c29302b3c9ea6
Arnel Pällo <arnel@pactum.com>
2023-02-16 00:32:38 +0200

    better env management

--- settings.py
+++ settings.py
@@ -1,6 +1,17 @@
+import os
+
 from dotenv import load_dotenv
 
 
 def load_env():
-    load_dotenv("overrides.env")
+    overrides = os.environ.get("overrides")
+    if overrides is not None:
+        if load_dotenv(overrides):
+            print(f"Overriding settings with values from {overrides}")
+
     load_dotenv("defaults.env")
+
+    settingsfile = os.environ.get("settingsfile")
+    if settingsfile is not None:
+        if load_dotenv(settingsfile):
+            print(f"Loading more settings from {settingsfile}")
