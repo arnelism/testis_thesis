5c5ed73f2c0a5b3d3a7a68f44534091be13c4b5b
Arnel Pällo <arnel@pactum.com>
2023-04-24 21:22:52 +0300

    Models registry

---
+++ available_models.py
@@ -0,0 +1,11 @@
+available_models = {
+    (1, 1, "color"):    "allall_level1_overlap1_color_2023-04-23_1025",
+    (1, 10, "color"):   "allall_level1_overlap10_color_2023-03-01_0300",
+    (1, 30, "color"):   "allall_level1_overlap30_color_2023-04-22_1551",
+
+    (2, 1, "color"):    "allall_level2_overlap1_color_2023-04-23_1941",
+    (2, 10, "color"):   "allall_level2_overlap10_color_2023-03-02_0348",
+    (2, 30, "color"):   "allall_level2_overlap30_color_2023-04-23_0106",
+
+    (1, 10, "grayscale"):    "allall_level1_overlap10_grayscale_2023-04-24_1450",
+}
